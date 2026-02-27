0cca4dbf1370945b2ebbeef74a535c4a930cbded
Parisharma16 <b22cs039@iitj.ac.in>
2026-02-28 03:22:26 +0530

    Refactored project structure

--- src/orchestrator.py
+++ src/orchestrator.py
@@ -1,32 +1,17 @@
-from register import register_student
-from mark_attendance import mark_attendance
-
+from initialize import register_student, mark_attendance
 
 class AttendanceOrchestrator:
 
     def __init__(self):
-        """
-        Orchestrator does not initialize models.
-        That is already handled inside init.py.
-        """
         pass
-
-    # ---------------------------------------------
 
     def register(self, image_path, roll, name):
         return register_student(image_path, roll, name)
 
-    # ---------------------------------------------
-
     def mark(self, group_photo_path, threshold=0.45):
         return mark_attendance(group_photo_path, threshold)
 
-    # ---------------------------------------------
-
     def run_pipeline(self, group_photo_path):
-        """
-        High-level function for full attendance workflow
-        """
         attendance = self.mark(group_photo_path)
 
         print("\nFinal Attendance List:")
